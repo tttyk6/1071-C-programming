a94c1b7a958ac5f33fd0eb0daca1873904402c8e
tttyk6 <43337038+tttyk6@users.noreply.github.com>
2018-12-03 18:26:07 +0800

    Add files via upload

---
+++ w13/swap.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+ void swap(int *pa,int *pb)
+    {
+    int temp;
+    temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+printf("swap : %d %d\n",*pa ,*pb);
+    }
+
+int main(){
+
+    int a=10, b=20;
+    swap (&a,&b);
+    printf("main : %d %d\n",a ,b);
+
+}
