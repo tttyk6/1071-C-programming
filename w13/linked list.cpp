f833a39d167623481ceec75fb8e035ce6c7842ae
tttyk6 <43337038+tttyk6@users.noreply.github.com>
2018-12-04 17:52:12 +0800

    Add files via upload

---
+++ w13/linked list.cpp
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+/*struct Date
+{
+    int year;
+    int month;
+    int day;
+};
+
+/* self-referential structure */
+struct ListNode {
+    int id;             // lotto id
+    Date date;   // opening date
+    int num[6];         // winning numbers
+    ListNode *nextPtr; /* pointer to next node*/
+}; /* end structure listNode */
