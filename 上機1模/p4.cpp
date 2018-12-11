2beb69e3680359a5c9e2634891cb1c10a44d1f79
tttyk6 <43337038+tttyk6@users.noreply.github.com>
2018-12-12 05:26:37 +0800

    Add files via upload

---
+++ 上機1模/p4.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+int a,b,c,d;
+
+while(1){
+        if(a=-1)
+        break;
+
+printf("Enter N (-1 to exit): ");
+scanf("%d%d%d%d",&a,&b,&c,&d);
+
+
+printf("encode(%d%d%d%d) = %d%d%d%d // by calling (N) ,a,b,c,d,   c,d,(a+5)%10,b");
+printf("decode(%d%d%d%d) = %d%d%d%d // by calling (N) ,c,d,(a+5)%10,b  ,a,b,c,d ");
+
+
+}
+printf("Enter N (-1 to exit): ");
+scanf("%d%d%d%d",&a,&b,&c,&d);
+
+
+}
---
+++ 上機1模/p5.cpp
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+void
+
+void
+
+void
+
+void
+
+int main(){
+
+printf("(1) Arithmetic Computation
+        (2) List BMI ranges
+        (3) Draw Four Vertical Triangles
+        (4) encode(n)
+        (5) decode(n)
+        (6) Exit ");
+
+
+        while(1){
+            switch(1){
+
+
+            }
+            switch(2){
+
+
+            }
+            switch(3){
+
+
+            }
+            switch(4){
+
+
+            }
+            switch(5){
+
+
+            }
+            switch(6)
+                break;
+
+            }
+            return 0;
+
+
+
+
+
+
+}
