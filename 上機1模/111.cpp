109525a6ea0f293083d5540987dbc95cb24b8550
tttyk6 <43337038+tttyk6@users.noreply.github.com>
2018-12-12 13:27:54 +0800

    Add files via upload

---
+++ 上機1模/111.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int main ()
+
+{
+    int h;
+    int wb,wf;
+    int bb,bf;
+
+    while(h!=-1)
+    {
+        printf("Enter height (-1 to exit):  ");
+        scanf("%d",&h);
+        bf=1;
+        wf=h;
+        for(int i=1; i<=h; i++)
+        {
+            for(wb=1; wb<wf; wb++)
+            {
+                printf("-");
+            }
+            for(bb=1; bb<=bf; bb++)
+            {
+                printf("*");
+            }
+            printf("--");
+            for(int j=1;j<=i;j++)
+            {
+                printf("*");
+            }
+            wf=wf-1;
+            bf=bf+1;
+            printf("\n");
+        }
+
+    }
+
+    printf("Enter height (-1 to exit):   ");
+
+    return 0;
+
+}
